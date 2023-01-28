6059d179bf7fe8e3a92c9bfc04c077fa7655f832
Glenn Salaman <43499190+gsalaman@users.noreply.github.com>
2023-01-28 11:51:39 -0700

    Create letters.py

---
+++ letters.py
@@ -0,0 +1,9 @@
+# This program shows how to extract letters from a given string.
+
+my_string = "hello"
+
+print("your string contains the following letters: ")
+
+for letter in my_string:
+  print(letter + " ")
+  
